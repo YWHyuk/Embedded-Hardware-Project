97a8a5324c08c40000651d96d18361366229b5cf
JANGDORRY <guswns531@gmail.com>
2019-05-02 16:49:30 +0900

    0502
    숫자 6이 뛰어넘어가는 문제가 존재.

---
+++ bcd_7seg.vhd
@@ -0,0 +1,62 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:28:44 04/30/2019 
+-- Design Name: 
+-- Module Name:    bcd_7seg - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity bcd_7seg is
+	port ( bcd_in : in STD_LOGIC_VECTOR (3 downto 0);
+			 sel_in : in STD_LOGIC_VECTOR (2 downto 0);
+			 Y : out STD_LOGIC_VECTOR (7 downto 0);
+			 sel_7segs : out STD_LOGIC_VECTOR (5 downto 0));
+end bcd_7seg;
+
+architecture Behavioral of bcd_7seg is
+
+begin
+	sel_7segs <= 	"000001" when sel_in = "000" else
+						"000010" when sel_in = "001" else
+						"000100" when sel_in = "010" else
+						"001000" when sel_in = "011" else
+						"010000" when sel_in = "100" else
+						"100000" when sel_in = "101" else
+						"000000";
+	
+	Y <= 	"00111111" when (bcd_in = "0000") else --1
+			"00000110" when (bcd_in = "0001") else --2
+			"01011011" when (bcd_in = "0010") else --3
+			"01001111" when (bcd_in = "0011") else --4
+			"01100110" when (bcd_in = "0100") else --5
+			"01101101" when (bcd_in = "0101") else --6 
+			"01111100" when (bcd_in = "0110") else --7 
+			"00000111" when (bcd_in = "1000") else --8
+			"01111111" when (bcd_in = "1001") else --9
+			"00000000";
+	
+end Behavioral;
+
---
+++ digit6_counter.vhd
@@ -0,0 +1,64 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:56:35 05/02/2019 
+-- Design Name: 
+-- Module Name:    digit6_counter - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity digit6_counter is
+    Port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
+           push : in  STD_LOGIC;
+           bcd0, bcd1, bcd2, bcd3, bcd4, bcd5 : out  STD_LOGIC_VECTOR (3 downto 0));
+end digit6_counter;
+
+architecture Behavioral of digit6_counter is
+	component counter_0to9 is
+		Port ( clk, rst : in  STD_LOGIC;
+			c_en : in std_logic;			-- enable counter
+			cnt : out  STD_LOGIC_VECTOR (3 downto 0);
+			c_up: out std_logic);	-- upper counter enable signal
+	end component;	
+	signal cup0, cup1, cup2, cup3, cup4, cup5 : std_logic;
+	signal cen1, cen2, cen3, cen4, cen5 : std_logic;
+
+begin
+	cen1 <= cup0 and push;
+	cen2 <= cup1 and cen1;
+	cen3 <= cup2 and cen2;
+	cen4 <= cup3 and cen3;
+	cen5 <= cup4 and cen4;
+
+	dig0: counter_0to9 port map (clk, rst, push, bcd0, cup0);
+	dig1: counter_0to9 port map (clk, rst, cen1, bcd1, cup1);
+	dig2: counter_0to9 port map (clk, rst, cen2, bcd2, cup2);
+	dig3: counter_0to9 port map (clk, rst, cen3, bcd3, cup3);
+	dig4: counter_0to9 port map (clk, rst, cen4, bcd4, cup4);
+	dig5: counter_0to9 port map (clk, rst, cen5, bcd5, cup5);
+
+end Behavioral;
+
